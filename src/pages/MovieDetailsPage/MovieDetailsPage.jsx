d4cc2c371b6639f23ad991711e5f69e9b5c75ff8
PandaTrumpet <dsavontrumpet@gmail.com>
2024-04-28 12:43:10 +0300

    update

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -28,23 +28,13 @@ export default function MovieDetailsPage() {
     fetchMovie();
   }, [moviesId]);
   const location = useLocation();
-  const backLinkURL = useRef(location.state?.from ?? "/movies");
+  const backLinkURL = useRef(location.state ?? "/movies");
 
   console.log(backLinkURL);
 
   const defaultImg =
     "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
-  {
-    /* <img src={
- movieData.poster_path ?
- 
- : defaultImg
-}
-width={250}
-alt="poster"
-/> */
-  }
   const mainPhotoURL = `https://image.tmdb.org/t/p/w500${dataMovie.backdrop_path}`;
   return (
     <div>
